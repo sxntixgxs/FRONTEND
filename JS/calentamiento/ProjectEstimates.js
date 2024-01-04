820c6f459402a1568907daeca23cf55a25c3fe9f
SantiagoSandoval <sandovalbgasantiago@gmail.com>
2024-01-03 20:02:01 -0500

    revisar ejercicio

---
+++ JS/calentamiento/ProjectEstimates.js
@@ -0,0 +1,18 @@
+function projectEstimates(size,projectCosts,target){
+    let parejasCreadas=[]
+    let output=0
+    for(i=0;i<size;i++){
+        pareja=[projectCosts[i],projectCosts[i+1]]
+        parejasCreadas.push(pareja)
+        if(target===Math.abs(pareja[0]-pareja[1])){
+            output+=1
+        }
+    }
+    console.log("output: ",output)
+    console.log(parejasCreadas)
+}
+let size=5;
+let projectCosts=[1,5,3,4,2]
+let target=2;
+projectEstimates(size,projectCosts,target)
+//revisar y hacer con forEach para que recorra todos los elementos hagan un recorrido
