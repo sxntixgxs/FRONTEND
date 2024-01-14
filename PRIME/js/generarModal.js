66c95135c732bd57b7c4aabc5ae38cbc1036b739
SantiagoSandoval <sandovalbgasantiago@gmail.com>
2024-01-13 21:24:06 -0500

    imgs

---
+++ PRIME/js/generarModal.js
@@ -0,0 +1,20 @@
+
+function generarTarjeta(){
+    const tarjeta = document.createElement("div");
+    const img = document.createElement("img");
+    const desc = document.createElement("p");
+    const btn_stats = document.createElement("button")
+    const btn_vote = document.createElement("button")
+    const modal = document.getElementById("modal")
+    let i = 1;
+    while(i<7){
+        tarjeta.classList.add("card")
+        img.classList.add("album1")
+        img.src = 
+
+
+
+        i++
+    }
+
+}
